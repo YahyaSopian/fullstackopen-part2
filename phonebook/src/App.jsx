dc514c19f22451d0a0fdf40151580d2c25368609
Yahya Sopian <yahyasopian5@gmail.com>
2024-12-08 01:33:50 +0700

    Menyelesaikan latihan part 2 - 2.6 -handbook langkah 1

---
+++ phonebook/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    console.log("Button clicked", event.target);
+    // Add new name to state persons
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+  const handlePersonChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {/* Displaying person */}
+      <ul>
+        {persons.map((person) => (
+          <Person key={person.name} person={person} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;

71b4cfb0e04f208d87e23be2a8cc63f3efb23cb4
Yahya Sopian <yahyasopian5@gmail.com>
2024-12-08 15:04:29 +0700

    Menambahkan fitur filter untuk menyelesaikan latihan part 2 - 2.9 -handbook step 4

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,9 +2,15 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "99075675" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -14,7 +20,7 @@ const App = () => {
       name: newName,
       number: newNumber,
     };
-    // Mengecek apakah nama yang diinput sudah ada ataukan belum
+    // Cek apakah nama sudah ada di buku telepon
     const existName = persons.some((person) => person.name === newName);
 
     if (existName) {
@@ -33,10 +39,20 @@ const App = () => {
     console.log(event.target.value);
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setFilter(event.target.value);
+  };
 
+  const personsToShow = persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()));
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shawn with <input value={filter} onChange={handleFilterChange} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -51,7 +67,7 @@ const App = () => {
       <h2>Numbers</h2>
       {/* Displaying person */}
       <ul>
-        {persons.map((person) => (
+        {personsToShow.map((person) => (
           <Person key={person.name} person={person} />
         ))}
       </ul>

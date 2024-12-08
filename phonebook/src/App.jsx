f7455dcb4b6beb7cf6128b19b7939398da73fb7e
Yahya Sopian <yahyasopian5@gmail.com>
2024-12-08 13:44:15 +0700

    Menambahkan number untuk menyelesaikan latihan part 2 - 2.8 -handbook step 3

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import Person from "./components/Person";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "99075675" }]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     console.log("Button clicked", event.target);
-    // Add new name to state persons
+    // Add new name and number to state persons
     const personObject = {
       name: newName,
+      number: newNumber,
     };
     // Mengecek apakah nama yang diinput sudah ada ataukan belum
     const existName = persons.some((person) => person.name === newName);
@@ -20,11 +22,16 @@ const App = () => {
     } else {
       setPersons(persons.concat(personObject));
       setNewName("");
+      setNewNumber("");
     }
   };
-  const handlePersonChange = (event) => {
+  const handleNameChange = (event) => {
     console.log(event.target.value);
     setNewName(event.target.value);
+  };
+  const handleNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -32,7 +39,10 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handlePersonChange} />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
